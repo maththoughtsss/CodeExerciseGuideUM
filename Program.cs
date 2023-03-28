2393b8525a64e1ed7ae89810d2353df445fb4537
maththoughtsss <jonathan.munoz01@mayor.cl>
2023-03-28 01:36:01 -0300

    Cálculador de IVA

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@
             int numProject;
 
             Console.WriteLine("******* Bienvenido al Proyecto Guía de Ejercicios *******");
-            Console.WriteLine("Favor ingrese el proyecto a utilizar: \n \n 1.- Perímetro y Área de un Rectángulo");
+            Console.WriteLine("Favor ingrese el proyecto a utilizar: \n 1.- Perímetro y Área de un Rectángulo \n 2.- Cálculador de IVA");
             strProject = Console.ReadLine();
 
             int.TryParse(strProject, out numProject);
@@ -20,6 +20,10 @@
             if(numProject == 1)
             {
                 RectangleCalculator();
+            }
+            if(numProject == 2)
+            {
+                IVACalculator();
             }
             else {
                 Console.WriteLine("Proyecto No Encontrado, Intente Nuevamente.");
@@ -42,8 +46,32 @@
             perimeter = 2*length + 2*width;
             area = length * width;
 
+            Console.WriteLine("++++++++++ ++++++++++ ++++++++++");
             Console.WriteLine($"Perímetro: {perimeter} u");
             Console.WriteLine($"Área: {area} u2");
+            Console.WriteLine("++++++++++ ++++++++++ ++++++++++");
+        }
+        static void IVACalculator()
+        {
+            string? strPrice;
+            double price, iva,priceIVA, priceWithoutIVA;
+
+            iva = 0.19;
+
+            Console.WriteLine("Cálculo del IVA al Valor de un Producto o Servicio");
+            Console.WriteLine("Ingrese el precio del producto o servicio: ");
+            strPrice = Console.ReadLine();
+
+            double.TryParse(strPrice, out price);
+
+            priceIVA = price * iva;
+            priceWithoutIVA = price - priceIVA;
+
+            Console.WriteLine("++++++++++ ++++++++++ ++++++++++");
+            Console.WriteLine($"Total Precio IVA = {priceIVA}");
+            Console.WriteLine($"Total Precio Sin IVA = {priceWithoutIVA}");
+            Console.WriteLine($"Total Precio = {price}");
+            Console.WriteLine("++++++++++ ++++++++++ ++++++++++");
         }
     }
 }
